1fb99da9b34eb390dffa33e9502312f956a216ee
Pintta <69728770+Pintta@users.noreply.github.com>
2023-10-04 21:52:21 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,9 @@ game 'gta5'
 lua54 'yes'
 files {
   'ui/index.html',
-  'ui/style.css',
-  --'ui/script.js'
+  --'ui/style.css', -- If you use style, you need made style.css file and write link to index.html file code.
+  --'ui/script.js' -- If you use script, you need made script.js file and write link to index.html file code.
 }
 loadscreen 'html/index.html'
---loadscreen_cursor 'yes'
+--loadscreen_cursor 'yes' -- If you want use cursor
 loadscreen_manual_shutdown 'yes'
