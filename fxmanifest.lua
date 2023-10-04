ce130cf4aae9fcb9a962ebdf9fd3e6d5330c861d
Pintta <69728770+Pintta@users.noreply.github.com>
2023-10-04 21:49:51 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+files {
+  'ui/index.html',
+  'ui/style.css',
+  --'ui/script.js'
+}
+loadscreen 'html/index.html'
+--loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
